7d11fee83ab654b7af221033d81683a19f6ef99f
Anton Bryzgaloff (aka Mashkoff) <tony.bryzgaloff@mail.ru>
2017-01-15 04:15:46 +0300

    Bug fix on measuring.sh

--- measuring.sh
+++ measuring.sh
@@ -5,7 +5,7 @@ do
     total=0
     for j in {1..7}  # усреднение из 7 запусков
     do
-        cmd="$(echo -e "$i\n12" | python $1)"
+        cmd="$(echo -e "12\n$i" | python $1)"
         # 12 - время работы магазина в часах
         cur="$(echo "$cmd" | grep "Время ожидания составило" | sed 's/[^0-9]//g')"
         total=$((total + cur))
